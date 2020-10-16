a0df68409f1b7ebf4d714ff2a8b7a9ef83de5c55
haha <1234>
2020-10-16 09:40:23 +0800

    Initial commit

---
+++ src/main/java/com/beans/MyUser.java
@@ -0,0 +1,18 @@
+package com.beans;
+
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName(value = "myuser")
+public class MyUser {
+
+
+
+
+
+
+}
---
+++ src/test/java/Test1.java
@@ -0,0 +1,55 @@
+import com.App;
+import com.beans.MyUser;
+import com.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class Test1 {
+
+    @Autowired
+    private UserMapper userMapper;
+
+   // @Autowired
+   // private IUserService userService;
+
+    @Test
+    public void test1(){
+//        List<MyUser> list = userMapper.selectList(null);
+////        System.out.println(list);
+
+        System.out.println("ok");
+    }
+
+    @Test
+    public void test2(){
+
+    }
+
+    @Test
+    public void save() {
+        MyUser user = new MyUser();
+        userMapper.insert(user);
+    }
+    @Test
+    public void update() {
+        MyUser user = new MyUser();
+        userMapper.updateById(user);
+    }
+
+    @Autowired
+    private MyUser u;
+    @Test
+    public void testMyUser() {
+        System.out.println(u);
+    }
+
+
+}
